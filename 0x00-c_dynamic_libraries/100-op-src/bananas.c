c731b2237485d7d22a35666fd2f6c1ee7334af52
Rona Chong <ronachong@gmail.com>
2017-06-09 18:08:08 -0700

    Add bananas func and test via new test main.

---
+++ 0x00-c_dynamic_libraries/100-op-src/bananas.c
@@ -0,0 +1,6 @@
+#include "100-op.h"
+
+char *bananas(char *s)
+{
+  return basefunc(" ,\n \\`.__.\n  `._,'\n", s);
+}
---
+++ 0x00-c_dynamic_libraries/100-op-src/test-mains/test-fruit.c
@@ -0,0 +1,10 @@
+#include "100-op.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+  printf("%s", apples("filler string"));
+  printf("%s", bananas("filler string"));
+  return (0);
+}
