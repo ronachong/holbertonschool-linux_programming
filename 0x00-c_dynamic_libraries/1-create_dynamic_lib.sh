e91e1e676d750c0c994d1d18824442f23e2682fe
Rona Chong <ronachong@gmail.com>
2017-06-11 14:34:15 -0700

    Revert 1-create_dynamic_lib to 1 liner.

--- 0x00-c_dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x00-c_dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-gcc -Wall -Werror -Wextra -pedantic -c -fPIC *.c
-gcc -Wall -Werror -Wextra -pedantic -shared -fPIC -o liball.so *.o
+gcc -Wall -Werror -Wextra -pedantic -shared -fPIC -o liball.so *.c
