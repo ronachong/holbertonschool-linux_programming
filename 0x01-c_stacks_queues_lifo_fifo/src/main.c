5854da8cd116b4ee26f148a9246227b9cf96872a
Rona Chong <ronachong@gmail.com>
2017-06-11 15:52:51 -0700

    Start iterating on main.c for monty.
    Handle case where no file arg is passed.

---
+++ 0x01-c_stacks_queues_lifo_fifo/src/main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  argv++;
+
+  if (argc < 2)
+    {
+      printf("USAGE: monty file\n");
+      return (EXIT_FAILURE);
+    }
+  printf("ran to completion\n");
+  return (EXIT_SUCCESS);
+}
