921a0488f4b519c2fe466bc2a61e7158da899ed7
Rona Chong <ronachong@gmail.com>
2017-06-15 12:48:50 -0700

    Add documentation to main.c

--- 0x01-ls/src/main.c
+++ 0x01-ls/src/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include "ls-components.h"
 
+/**
+ * main - executes hls with file arguments from command line
+ * @argc: number of command line arguments
+ * @argv: array of string arguments from command line
+ * Return: based on ls -
+ *     0      if OK,
+ *     1      if minor problems (e.g., cannot access subdirectory),
+ *     2      if serious trouble (e.g., cannot access command-line argument).
+ */
 int main(int argc, char **argv)
 {
 	int i;
