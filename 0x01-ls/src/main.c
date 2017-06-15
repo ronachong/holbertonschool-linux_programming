6a8fffe75a14172a8f56a221ccae829ab5f200a2
Rona Chong <ronachong@gmail.com>
2017-06-14 18:13:23 -0700

    Add preliminary (pseudo)code for main.c

--- 0x01-ls/src/main.c
+++ 0x01-ls/src/main.c
@@ -1,5 +1,19 @@
+#include <stdio.h>
 #include "ls-components.h"
 
-int main(int argc, char *argv)
+int main(int argc, char **argv)
 {
+	int i;
+
+	if (argc == 1)
+		return ls(/* cwd path */);
+	if (argc == 2)
+		return ls(argv[1]); /* to qualify path? */
+	for (i = 1; i < argc; i++)
+	{
+		printf("%s:\n", argv[i]);
+		ls(argv[i]); /* need to qualify path? */
+	}
+	/* compute return val */
+	return (0);
 }
