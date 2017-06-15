a33f29c16b18b6b45658bbadcfc84084c670ae37
Rona Chong <ronachong@gmail.com>
2017-06-14 17:59:55 -0700

    Fix prototypes/function declarations and structs.
    Remove names from return specifications in prototypes.
    Add missing struct keywords for struct fields.

--- 0x01-ls/src/ls-components.h
+++ 0x01-ls/src/ls-components.h
@@ -4,14 +4,14 @@
 typedef struct fname_s
 {
 	char *name;
-	fname_s *next;
+	struct fname_s *next;
 } fname_t;
 
 typedef struct finfo_s
 {
 	char *name;
 	int *size;
-	finfo_s *next;
+	struct finfo_s *next;
 } finfo_t;
 
 typedef struct vfinfo_s
@@ -23,16 +23,16 @@ typedef struct vfinfo_s
 	char *user;
 	char *owner;
 	char *date;
-	vfinfo_s *next;
+	struct vfinfo_s *next;
 } vfinfo_t;
 
 int ls(char *dpath);
-char **fpaths get_fpaths(DIR *dstrm, char *opxns);
-fname_t **fnames get_fnames(char **fpaths, char *opxns);
-int print_fnames(fnames_t **fnames, char *opxns);
-finfo_t **finfo get_finfo(char **fpaths, char *opxns);
+char **get_fpaths(DIR *dstrm, char *opxns);
+fname_t **get_fnames(char **fpaths, char *opxns);
+int print_fnames(fname_t **fnames, char *opxns);
+finfo_t **get_finfo(char **fpaths, char *opxns);
 int print_finfo(finfo_t **finfo, char *opxns);
-vfinfo_t **vfinfo get_vfinfo(char **fpaths, char *opxns);
+vfinfo_t **get_vfinfo(char **fpaths, char *opxns);
 int print_vfinfo(vfinfo_t **vfinfo, char *opxns);
 
 #endif
