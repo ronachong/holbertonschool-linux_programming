f1f473320bac6628794643b72ba2530c2f064ec1
Rona Chong <ronachong@gmail.com>
2017-06-14 19:02:32 -0700

    Add missing newline for output for multipe dirs; update comments.

--- 0x01-ls/src/main.c
+++ 0x01-ls/src/main.c
@@ -8,11 +8,14 @@ int main(int argc, char **argv)
 	if (argc == 1)
 		return ls("N/A"); /* change to cwd */
 	if (argc == 2)
-		return ls(argv[1]); /* to qualify path? */
+		return ls(argv[1]);
+	/* probably have to sort dir paths before lsing */
 	for (i = 1; i < argc; i++)
 	{
+		if (i > 1)
+			putchar('\n');
 		printf("%s:\n", argv[i]);
-		ls(argv[i]); /* need to qualify path? */
+		ls(argv[i]);
 	}
 	/* compute return val */
 	return (0);
