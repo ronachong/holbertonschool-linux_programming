51aaa992ac989a51f6a80d0c37d0def42e361f5b
Rona Chong <ronachong@gmail.com>
2017-07-19 14:33:17 -0700

    Iterate on proof of concept to take in args.

--- 0x03-proc_filesystem/check_mem.py
+++ 0x03-proc_filesystem/check_mem.py
@@ -1,7 +1,34 @@
 #!/usr/bin/python3
 
-with open("/proc/1930/mem", 'r') as mem:
-    mem.seek(0x00eb2000)
-    heap = mem.read(0x00ed3000 - 0x00eb2000)
-    print heap
+import sys
 
+def get_heap(pid):
+    """Identify the start and end addresses for the heap of a process."""
+    # TODO: handle any io errors?
+    with open("/proc/{pid}/maps".format(pid=pid), 'r') as maps:
+            while True:
+                mapping = maps.readline()
+                if not mapping:
+                    # EOF presumably reached
+                    sys.exit("Could not identify heap in /proc/{pid}/maps."
+                             .format(pid=pid))
+
+                if mapping[-8:] == ' [heap]\n':
+                    mapping = mapping.split('-')
+                    heap_start = int(mapping[0], 16)
+                    heap_end = int(mapping[1].split(' ')[0], 16)
+                    return (heap_start, heap_end)
+
+def main():
+    pid = sys.argv[1]
+    search = sys.argv[2]
+    replace = sys.argv[3]
+
+    heap_start, heap_end = get_heap(pid)
+
+    with open("/proc/{pid}/mem".format(pid=pid), 'r') as mem:
+        mem.seek(heap_start)
+        heap = mem.read(heap_end - heap_start)
+        print heap
+
+main()
