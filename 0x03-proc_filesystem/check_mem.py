d1fa3ee19f13dae171438116adc601c2d745ca66
Rona Chong <ronachong@gmail.com>
2017-07-19 13:38:41 -0700

    Update proof of concept to check new process.

--- 0x03-proc_filesystem/check_mem.py
+++ 0x03-proc_filesystem/check_mem.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-with open("/proc/2440/mem", 'r') as mem:
-    mem.seek(0x020d0000)
-    heap = mem.read(0x020d0000 - 0x020d0000)
+with open("/proc/1930/mem", 'r') as mem:
+    mem.seek(0x00eb2000)
+    heap = mem.read(0x00ed3000 - 0x00eb2000)
     print heap
 
