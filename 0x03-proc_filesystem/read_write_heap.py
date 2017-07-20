5c2c97172f805109434510bc060b0b9f238c88db
Rona Chong <ronachong@gmail.com>
2017-07-20 13:11:14 -0700

    Fix whitespace.

--- 0x03-proc_filesystem/read_write_heap.py
+++ 0x03-proc_filesystem/read_write_heap.py
@@ -2,6 +2,7 @@
 
 import sys
 
+
 def get_heap(pid):
     """Identify the start and end addresses for the heap of a process."""
     # TODO: handle any io errors?
@@ -10,14 +11,17 @@ def get_heap(pid):
                 mapping = maps.readline()
                 if not mapping:
                     # EOF presumably reached
-                    sys.exit("ERROR: Could not identify heap in /proc/{pid}/maps."
-                             .format(pid=pid))
+                    sys.exit(
+                        "ERROR: Could not identify heap in /proc/{pid}/maps."
+                        .format(pid=pid)
+                    )
 
                 if mapping[-8:] == ' [heap]\n':
                     mapping = mapping.split('-')
                     heap_start = int(mapping[0], 16)
                     heap_end = int(mapping[1].split(' ')[0], 16)
                     return (heap_start, heap_end)
+
 
 def main():
     """Replace a string in heap of given process."""
@@ -26,7 +30,6 @@ def main():
     replace = sys.argv[3]
 
     heap_start, heap_end = get_heap(pid)
-
 
     with open("/proc/{pid}/mem".format(pid=pid), 'r+b') as mem:
         mem.seek(heap_start)
